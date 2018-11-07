c3505afc3081513e35465d776431dd2498bb0f52
Bryant Barron <bwalt@Bryants-MacBook-Pro.local>
2018-11-07 04:00:18 -0800

    Completed Series

---
+++ ruby/series/series.rb
@@ -0,0 +1,22 @@
+class Series
+  def initialize(series)
+    @series = (series)
+  end
+
+  def slices(i)
+    substrings = []
+    series = @series
+
+    if i > @series.length
+      raise ArgumentError
+    end
+
+    until series.length < i
+      substrings << series[0..i-1]
+      series = series[1..-1]
+    end
+
+    substrings
+
+  end
+end
