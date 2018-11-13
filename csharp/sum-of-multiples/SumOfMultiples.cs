a97ddde32ebd208ecb629a70f874a37e1b0eee3e
Bryant Barron <bwalt@Bryants-MacBook-Pro.local>
2018-11-13 09:28:44 -0800

    Completed Sum of Multiples

---
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max)
+    {
+
+        return Enumerable.Range(1, max - 1)
+                         .Where(n => multiples
+                                .Any(multiple => multiple != 0 &&  n % multiple == 0)).Sum();
+    }
+}
