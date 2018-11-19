4729ec38f858c5a73f4b4a864af5c112c887fa38
Bryant Barron <bwalt@Bryants-MacBook-Pro.local>
2018-11-19 15:18:13 -0800

    Finished grains exercise

--- csharp/grains/Grains.cs
+++ csharp/grains/Grains.cs
@@ -1,14 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class Grains
 {
     public static ulong Square(int n)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        ulong square;
+
+        if(n < 1 || n > 64){
+            throw new ArgumentOutOfRangeException(nameof(n));
+        }
+
+        return square = (ulong)Math.Pow(2, n - 1);
     }
 
     public static ulong Total()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Enumerable.Range(1, 64).Select(Square).Aggregate((x, y) => x + y);
     }
 }
